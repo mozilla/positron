cb59cb9423687e63671e1ac84266042b4f51103b
Myk Melez <myk@mykzilla.org>
2016-04-12 14:46:16 -0700

    implement app.quit()

--- positron/modules/atom_browser_app.js
+++ positron/modules/atom_browser_app.js
@@ -4,7 +4,20 @@
 
 "use strict";
 
-exports.app = { /* stub */ };
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import('resource://gre/modules/Services.jsm');
+
+exports.app = {
+  quit: function() {
+    // XXX Emit the before-quit and will-quit events.
+    Services.startup.quit(Services.startup.eAttemptQuit);
+  },
+};
+
 exports.appendSwitch = function() { /* stub */ };
 exports.appendArgument = function() { /* stub */ };
 exports.dockBounce = function() { /* stub */ };
@@ -15,11 +28,6 @@ exports.hide = function() { /* stub */ };
 exports.show = function() { /* stub */ };
 exports.setMenu = function() { /* stub */ };
 exports.setIcon = function() { /* stub */ };
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
 
 // There isn't currently anything we need to do before emitting app.ready,
 // but apps will expect it to happen after a tick, so emit it in a timeout.
