678543e0bcea1ede5d7159f527df74ab6a7f9318
J. Ryan Stinnett <jryans@gmail.com>
2016-04-14 17:45:21 -0500

    Include DevTools client modules

--- positron/confvars.sh
+++ positron/confvars.sh
@@ -9,3 +9,6 @@ MOZ_CHROME_FILE_FORMAT=omni
 
 #XXXkhuey I have no idea why startupcache doesn't work
 MOZ_DISABLE_STARTUPCACHE=1
+
+# Include the DevTools client, not just the server (which is the default).
+MOZ_DEVTOOLS=all
