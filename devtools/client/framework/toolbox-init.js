7fb9923dade8cced3a6e02f0c1e30cc6c3fda056
J. Ryan Stinnett <jryans@gmail.com>
2016-04-15 16:26:51 -0500

    Destroy toolbox on host unload

--- devtools/client/framework/toolbox-init.js
+++ devtools/client/framework/toolbox-init.js
@@ -42,6 +42,7 @@ if (url.search.length > 1) {
   // Specify the default tool to open
   let tool = url.searchParams.get("tool");
 
+  let toolboxOpened;
   if (url.searchParams.has("target")) {
     // Attach toolbox to a given browser iframe (<xul:browser> or <html:iframe
     // mozbrowser>) whose reference is set on the host iframe.
@@ -64,22 +65,40 @@ if (url.search.length > 1) {
         DebuggerServer.addBrowserActors();
       }
       let client = new DebuggerClient(DebuggerServer.connectPipe());
-      Task.spawn(function*() {
+      toolboxOpened = Task.spawn(function*() {
         yield client.connect();
         // Creates a target for a given browser iframe.
         let response = yield client.getTab({ tab });
         let form = response.tab;
-        let target = yield TargetFactory.forRemoteTab({client, form, chrome: false});
+        let target = yield TargetFactory.forRemoteTab({
+          client,
+          form,
+          chrome: false
+        });
         let options = { customIframe: host };
-        yield gDevTools.showToolbox(target, tool, Toolbox.HostType.CUSTOM, options);
+        return gDevTools.showToolbox(target, tool, Toolbox.HostType.CUSTOM,
+                                     options);
       });
     }
   } else {
-    targetFromURL(url).then(target => {
+    toolboxOpened = Task.spawn(function*() {
+      let target = yield targetFromURL(url);
       let options = { customIframe: host };
-      return gDevTools.showToolbox(target, tool, Toolbox.HostType.CUSTOM, options);
-    }).then(null, e => {
-      window.alert("Unable to start the toolbox:" + e.message);
+      return gDevTools.showToolbox(target, tool, Toolbox.HostType.CUSTOM,
+                                   options);
     });
   }
+
+  toolboxOpened.catch(e => {
+    window.alert("Unable to start the toolbox: " + e.message);
+  });
+
+  // Ensure the toolbox gets destroyed if the host unloads
+  toolboxOpened.then(toolbox => {
+    let hostUnload = () => {
+      host.contentWindow.removeEventListener("unload", hostUnload);
+      toolbox.destroy();
+    };
+    host.contentWindow.addEventListener("unload", hostUnload);
+  });
 }
