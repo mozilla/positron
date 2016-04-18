863e08318b8164b17cbbcbe4fc5ae155087731a2
Myk Melez <myk@mykzilla.org>
2016-04-18 16:47:57 -0700

    implement v8_util.getHiddenValue/setHiddenValue

--- positron/modules/atom_common_v8_util.js
+++ positron/modules/atom_common_v8_util.js
@@ -4,5 +4,26 @@
 
 "use strict";
 
-exports.setHiddenValue = function() { /* stub */ };
-exports.getHiddenValue = function() { /* stub */ };
+// getHiddenValue/setHiddenValue attach properties to JavaScript objects
+// that are inaccessible except via the V8 API.  We emulate that API by storing
+// these values in a weak map of JavaScript objects to maps of property names
+// to property values.
+
+const hiddenValueMaps = new WeakMap();
+
+exports.getHiddenValue = function(obj, name) {
+  const hiddenValueMap = hiddenValueMaps.get(obj);
+  if (hiddenValueMap) {
+    return hiddenValueMap.get(name);
+  }
+  return undefined;
+};
+
+exports.setHiddenValue = function(obj, name, val) {
+  let hiddenValueMap = hiddenValueMaps.get(obj);
+  if (!hiddenValueMap) {
+    hiddenValueMap = new Map();
+    hiddenValueMaps.set(obj, hiddenValueMap);
+  }
+  hiddenValueMap.set(name, val);
+};
